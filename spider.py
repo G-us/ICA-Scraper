7b986278e06ad91d341badd37ded3160647534ad
henryp <henrygparsons@gmail.com>
2023-03-27 18:47:07 +0200

    YAY, it can distinguish between a product with a ursprungsland and not

--- spider.py
+++ spider.py
@@ -4,7 +4,7 @@ class Spider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
-        urls = [    'https://handlaprivatkund.ica.se/stores/1004584/products/Bryggkaffe-Mellanrost-500g-Arvid-Nordquist-Classic/1004503',
+        urls = [    'https://handlaprivatkund.ica.se/stores/1004584/products/Kalkon-R%C3%B6kt-tunna-skivor-200g-P%C3%A4rsons/1026006',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.on_response)
@@ -12,7 +12,7 @@ class Spider(scrapy.Spider):
     def on_response(self, response):
 
       ThirdDiv = response.xpath("/html/body/div[1]/div/div[1]/div[2]/main/div/div[2]/div/div/div/div[3]/h2").get()
-      if ThirdDiv.___contains___("Ingredienser"):
+      if "Ingredienser" in ThirdDiv:
         
         print("No Ursprungsland")
         ingredients = response.xpath("/html/body/div[1]/div/div[1]/div[2]/main/div/div[2]/div/div/div/div[3]/div/text()").get()
@@ -20,7 +20,6 @@ class Spider(scrapy.Spider):
         print("Ursprungsland")
         ingredients = response.xpath("/html/body/div[1]/div/div[1]/div[2]/main/div/div[2]/div/div/div/div[4]/div/text()").get()
 
-      print("This is the 3 header: " + response.xpath("/html/body/div[1]/div/div[1]/div[2]/main/div/div[2]/div/div/div/div[3]/h2/text").get())
       print(ingredients)
 
       
